3a17785e0e54184a6e10d73d21bfa43251029ca8
takke <takke30@gmail.com>
2020-10-21 13:30:09 +0900

    build.gradle.kts

---
+++ recyclerviewfastscroller/build.gradle.kts
@@ -0,0 +1,19 @@
+import dependencies.Dep
+
+plugins {
+    id("com.android.library")
+}
+
+// build.gradle.kts から rootProject.ext.compileSdkVersion 等にアクセスできないので別ファイルに切り出す
+apply(from = "build_android.gradle")
+
+dependencies {
+    implementation(Dep.Library.androidx_recyclerview)
+}
+
+// Add the below when ready to publish to maven
+//apply from: 'gradle-maven-push.gradle'
+//
+//afterEvaluate {
+//    androidJavadocs.classpath += project.android.libraryVariants.toList().first().javaCompile.classpath
+//}
